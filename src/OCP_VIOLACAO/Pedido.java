20fade32c241f485bad3e054667d3122725a3d72
DANIEL MULLER <daniel_muller150@estudante.sesisenai.org.br>
2025-11-26 16:27:42 -0300

    DM-2 feat: Fazendo a classe pedido

---
+++ src/OCP_VIOLACAO/Pedido.java
@@ -0,0 +1,43 @@
+package OCP_VIOLACAO;
+
+public class Pedido {
+    private String id;
+    private double valorBruto;
+    private int quantidadeItens;
+    private boolean isPago;
+    private String emailCliente;
+    private TipoDesconto tipoDesconto; // Tipo de desconto a ser aplicado
+
+
+    public Pedido(String id, double valorBruto, int quantidadeItens, String
+            emailCliente, TipoDesconto tipoDesconto) {
+        this.id = id;
+        this.valorBruto = valorBruto;
+        this.quantidadeItens = quantidadeItens;
+        this.isPago = false;
+        this.emailCliente = emailCliente;
+        this.tipoDesconto = tipoDesconto;
+    }
+
+    // --- GETTERS ---
+    public String getId() { return id; }
+    public double getValorBruto() { return valorBruto; }
+    public int getQuantidadeItens() { return quantidadeItens; }
+    public boolean isPago() { return isPago; }
+    public String getEmailCliente() { return emailCliente; }
+    public TipoDesconto getTipoDesconto() { return tipoDesconto; }
+
+    // --- SETTERS ---
+    public void setValorBruto(double valorBruto) { this.valorBruto = valorBruto; }
+    public void setQuantidadeItens(int quantidadeItens) { this.quantidadeItens =
+            quantidadeItens; }
+    public void setPago(boolean pago) { isPago = pago; }
+    public void setTipoDesconto(TipoDesconto tipoDesconto) { this.tipoDesconto =
+            tipoDesconto; }
+}
+
+    // Enum para Tipos de Desconto
+    enum TipoDesconto {
+      CUPOM, VIP, SAZONAL, ANIVERSARIO // ANIVERSARIO é o novo desconto que causará a modificação
+    }
+
